0b5337ea307af11422b34119dc0f562840f88bdf
Lubaiyana Afroze Khan <220138814+Lubaiyana@users.noreply.github.com>
2026-08-24 00:12:03 +0600

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,9 +1,46 @@
 <nav class="navbar">
+
     <a class="brand" href="dashboard.php">
         Dorm Management
     </a>
+
     <div class="nav-links">
-        <a href="dashboard.php">Dashboard</a>
-        <a href="logout.php" class="nav-logout">Log Out</a>
+
+        <a href="dashboard.php">
+            Dashboard
+        </a>
+
+
+        <?php if (
+            isset($_SESSION['role']) &&
+            $_SESSION['role'] === 'student'
+        ): ?>
+
+            <a href="maintenance_submit.php">
+                New Request
+            </a>
+
+            <a href="maintenance_list.php">
+                My Requests
+            </a>
+
+
+        <?php elseif (
+            isset($_SESSION['role']) &&
+            $_SESSION['role'] === 'admin'
+        ): ?>
+
+            <a href="admin_maintenance.php">
+                Maintenance
+            </a>
+
+        <?php endif; ?>
+
+
+        <a href="logout.php" class="nav-logout">
+            Log Out
+        </a>
+
     </div>
+
 </nav>
