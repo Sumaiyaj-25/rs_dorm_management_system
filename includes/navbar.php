1f4cea7941ca0f46a9c5e742bd2019d96358e4a0
Ash <ashfarahman201235@gmail.com>
2026-08-25 02:32:43 +0600

    Updated navbar for parcel management

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,9 +1,59 @@
 <nav class="navbar">
+
     <a class="brand" href="dashboard.php">
         Dorm Management
     </a>
+
     <div class="nav-links">
-        <a href="dashboard.php">Dashboard</a>
-        <a href="logout.php" class="nav-logout">Log Out</a>
+
+        <a href="dashboard.php">
+            Dashboard
+        </a>
+
+        <?php if (
+            isset($_SESSION['role']) &&
+            $_SESSION['role'] === 'student'
+        ): ?>
+
+            <a href="maintenance_submit.php">
+                New Request
+            </a>
+
+            <a href="maintenance_list.php">
+                My Requests
+            </a>
+
+            <a href="parcel_list.php">
+    My Parcels
+</a>
+
+<a href="verify_parcel.php">
+    Collect Parcel
+</a>
+
+        <?php elseif (
+            isset($_SESSION['role']) &&
+            $_SESSION['role'] === 'admin'
+        ): ?>
+
+            <a href="admin_maintenance.php">
+                Maintenance
+            </a>
+
+            <a href="admin_parcel.php">
+                Add Parcel
+            </a>
+
+            <a href="admin_parcel_list.php">
+                Manage Parcels
+            </a>
+
+        <?php endif; ?>
+
+        <a href="logout.php" class="nav-logout">
+            Log Out
+        </a>
+
     </div>
+
 </nav>
