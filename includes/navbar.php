24e49ae87dec4d435b0c9decbe020abc6814488e
Sumaiyaj-25 <sumaiyaj573@gmail.com>
2026-08-26 00:41:14 +0600

    Fix navbar link placement and remove duplicates

--- includes/navbar.php
+++ includes/navbar.php
@@ -37,14 +37,13 @@
         <?php endif; ?>
 
 
+        <a href="meals.php">
+            Meals
+        </a>
+
         <a href="logout.php" class="nav-logout">
             Log Out
         </a>
 
-    </div>
-
-        <a href="dashboard.php">Dashboard</a>
-        <a href="meals.php">Meals</a>
-        <a href="logout.php" class="nav-logout">Log Out</a>
     </div>
 </nav>
